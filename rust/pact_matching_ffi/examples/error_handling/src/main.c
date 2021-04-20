ef24958c64fa2948c70da828fd11cb4b8dc37754
Andrew Lilley Brinker <abrinker@mitre.org>
2021-04-20 10:33:25 -0700

    Another example, and fixes

--- rust/pact_matching_ffi/examples/error_handling/src/main.c
+++ rust/pact_matching_ffi/examples/error_handling/src/main.c
@@ -6,23 +6,25 @@
 #define ERROR_MSG_LEN 256
 
 int main(void) {
-    logger_init();
-    logger_attach_sink("stdout", LevelFilter_Trace);
-    logger_apply();
+    /*=======================================================================
+     * Simple empty message creation.
+     *---------------------------------------------------------------------*/
 
     Message *msg = message_new();
 
     if (msg == NULL) {
         char error_msg[ERROR_MSG_LEN];
-
         int error = get_error_message(error_msg, ERROR_MSG_LEN);
-
         printf("%s\n", error_msg);
-
         return EXIT_FAILURE;
     }
 
     message_delete(msg);
+
+
+    /*=======================================================================
+     * Creating a message from a JSON string.
+     *---------------------------------------------------------------------*/
 
     char *json_str = "{\
         \"description\": \"String\",\
@@ -31,12 +33,14 @@ int main(void) {
     }";
     Message *msg_json = message_new_from_json(0, json_str, PactSpecification_V3);
 
-    if (NULL == msg_json) {
+    if (msg_json == NULL) {
         char error_msg[ERROR_MSG_LEN];
         int error = get_error_message(error_msg, ERROR_MSG_LEN);
         printf("%s\n", error_msg);
         return EXIT_FAILURE;
     }
+
+    message_delete(msg_json);
 
     return EXIT_SUCCESS;
 }
